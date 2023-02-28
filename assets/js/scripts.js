00f4aad5c73a6877b8a2b4ebc136762e512c33ed
Maxime Hinderschiette <maxhin94@gmail.com>
2023-02-28 22:34:08 +0100

    [CONFIANCE] -> Add confiance part
    slider splide, titre

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,15 @@ $(window).scroll(function() {
     let pos = $(window).scrollTop();
 
     if(pos <= posText){
-        scrollText.css('margin-left', `-${pos/2}px`);
+        scrollText.css('margin-left', `-${pos/1}px`);
     }
 
 });
+
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    padding: '10rem',
+    autoplay: true
+} );
+
+splide.mount();
